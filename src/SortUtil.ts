c8225ce0ee63cf310e09e0ac32964f27aac861b6
Artin Peyvasteh <aripey11@gmail.com>
2021-02-18 22:14:48 -0800

    did it Armaan's way

--- src/SortUtil.ts
+++ src/SortUtil.ts
@@ -1,6 +1,6 @@
-import { LinkedListGroup } from './linkedlistgroup'
-import { NumberGroup } from "./numbergroup"
-import { CharacterGroup } from './charactergroup'
+import { LinkedListGroup } from './LinkedListGroup';
+import { NumberGroup } from "./NumberGroup";
+import { CharacterGroup } from './CharacterGroup';
 
 export class SortUtil {
     collection: NumberGroup | CharacterGroup | LinkedListGroup;
@@ -16,36 +16,13 @@ export class SortUtil {
       while (!isSorted) {
         isSorted = true;
         for (let i = 0; i < lastUnsorted; i++) {
-          // HANDLE LINKED LIST LOGIC HERE 
-          if (this.collection instanceof LinkedListGroup) {
+   
                 for (let y = 0; y < (length -1); y++) {
                     if (this.collection.compare(y, (y+1)) == true) {
                         this.collection.swap(y, y+1);
                     }
                 }
-                
-          }
-  
-          // HANDLE LIST OF NUMBERS LOGIC HERE
-          // I GOT IT TO WORK WITH ARRAY OF NUMBERS
-          if (this.collection instanceof Array) {
-            for (let y = 0; y < (length -1); y++) {
-                const nG = new NumberGroup(this.collection)
-                if (nG.compare(y, (y+1)) == true) {
-                    nG.swap(y, y+1);
-                }
-            }
-          }
-  
-          // HANDLE STRING LOGIC HERE
-          if (typeof this.collection === "string") {
-            for (let y = 0; y < (length -1); y++) {
-                const cG = new CharacterGroup(this.collection)
-                if (cG.compare(y, (y+1)) == true) {
-                    cG.swap(y, y+1);
-                }
-            }
-          }
+              
         }
         lastUnsorted--;
       }
